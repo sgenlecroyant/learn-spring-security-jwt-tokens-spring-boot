1f209e6640762ad155207a9c0de0fd49fb631907
sgenlecroyant <sgenlecroyant@gmail.com>
2022-06-17 23:36:46 +0200

    set up the project

---
+++ src/main/java/com/sgenlecroyant/security/MasteringSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.sgenlecroyant.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteringSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteringSpringSecurityApplication.class, args);
+	}
+
+}
