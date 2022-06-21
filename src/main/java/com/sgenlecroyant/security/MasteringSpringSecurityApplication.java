71629da30bafd0e6d0a0d1d46c2641644985cacf
sgenlecroyant <sgenlecroyant@gmail.com>
2022-06-22 00:32:32 +0200

    unblocked the H2 web frames

--- src/main/java/com/sgenlecroyant/security/MasteringSpringSecurityApplication.java
+++ src/main/java/com/sgenlecroyant/security/MasteringSpringSecurityApplication.java
@@ -7,9 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.sgenlecroyant.security.book.entity.Book;
 import com.sgenlecroyant.security.book.repository.BookRepository;
