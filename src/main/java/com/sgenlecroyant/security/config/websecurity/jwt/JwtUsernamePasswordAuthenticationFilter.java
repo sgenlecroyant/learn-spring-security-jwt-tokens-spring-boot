a35a1d422137e3a7c91664d221173c9fed0da11d
sgenlecroyant <sgenlecroyant@gmail.com>
2022-06-22 12:31:18 +0200

    implemented 'Generating JWT token on Authentication Success'

--- src/main/java/com/sgenlecroyant/security/config/websecurity/jwt/JwtUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/sgenlecroyant/security/config/websecurity/jwt/JwtUsernamePasswordAuthenticationFilter.java
@@ -1,20 +1,27 @@
 package com.sgenlecroyant.security.config.websecurity.jwt;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.UUID;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 public class JwtUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 
@@ -44,12 +51,25 @@ public class JwtUsernamePasswordAuthenticationFilter extends UsernamePasswordAut
 	@Override
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
 			Authentication authResult) throws IOException, ServletException {
-		System.out.println("Auth Success");
+		String someSecretKey = "helloworld helloworld helloworld helloworld";
+		String token = Jwts.builder()
+			.setSubject(authResult.getName())
+			.claim("authorities", authResult.getAuthorities())
+			.setIssuer("Supreme")
+			.setIssuedAt(new Date())
+			.setAudience("Supreme API consumer")
+			.setId(UUID.randomUUID().toString())
+			.signWith(Keys.hmacShaKeyFor(someSecretKey.getBytes(StandardCharsets.UTF_8)))
+			.setExpiration(java.sql.Date.valueOf(LocalDate.now().plusDays(2)))
+			.compact();
+		
+		response.addHeader(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", token));
+		
 	}
 
 	@Override
 	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException failed) throws IOException, ServletException {
-		System.out.println("Auth Failed");
+		System.out.println("Auth Failed: You can perform other business logic here, like brute forcing prevention, ...");
 	}	
 }
